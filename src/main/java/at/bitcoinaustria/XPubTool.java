e5d952fda7697f1317bb912494a6f3ce9521236c
Andreas Petersson <andreas@petersson.at>
2019-12-17 13:59:39 +0100

    make generateReceiveAddresses accessible

--- src/main/java/at/bitcoinaustria/XPubTool.java
+++ src/main/java/at/bitcoinaustria/XPubTool.java
@@ -1,3 +1,5 @@
+package at.bitcoinaustria;
+
 import org.bitcoinj.core.Address;
 import org.bitcoinj.core.LegacyAddress;
 import org.bitcoinj.crypto.ChildNumber;
@@ -5,9 +7,13 @@ import org.bitcoinj.crypto.DeterministicKey;
 import org.bitcoinj.crypto.HDKeyDerivation;
 import org.bitcoinj.params.MainNetParams;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class XPubTool {
 
     public static final String SAMPLE = "xpub6BkJWUzGLCNpraMs78UmJtU388Z6SkTABhmeRxrew8mYpWCf7ah4pVLWwtSR8KpESsiknB5CNqSYGSmLzRBUYY7qhSXnfeCjJU2hpLhghwr";
+    public static final int RECEIVE_PATH = 0;
 
     /*
     sample seed:
@@ -20,18 +26,28 @@ public class XPubTool {
 
      */
     public static void main(String[] args) {
-        String xPub = SAMPLE;
-        generateAddresses(xPub);
+        List<Address> addresses = generateReceiveAddresses(SAMPLE, 0, 1000);
+        for (int i = 0; i < addresses.size(); i++) {
+            Address address = addresses.get(i);
+            System.out.println("address \t" + i + "\t" + address);
+        }
     }
 
-    private static void generateAddresses(String xPub) {
+    public static List<Address> generateReceiveAddresses(String xPub, int offset, int count) {
+        List<Address> generated = new ArrayList<>();
         DeterministicKey pubKey = DeterministicKey.deserializeB58(xPub, MainNetParams.get());
-        DeterministicKey receiveAddressesPub = derivePub(pubKey, 0);
-        for (int i = 0; i < 1000; i++) {
-            DeterministicKey paymentAddr = derivePub(receiveAddressesPub, i);
-            Address addr = LegacyAddress.fromPubKeyHash(MainNetParams.get(),paymentAddr.getPubKeyHash());
-            System.out.println("address \t" + i + "\t" + addr);
+        DeterministicKey receiveAddressesPub = derivePub(pubKey, RECEIVE_PATH);
+        for (int i = offset; i < count; i++) {
+            Address addr = getAddress(receiveAddressesPub, i);
+            generated.add(addr);
         }
+        return generated;
+    }
+
+
+    private static Address getAddress(DeterministicKey receiveAddressesPub, int i) {
+        DeterministicKey paymentAddr = derivePub(receiveAddressesPub, i);
+        return LegacyAddress.fromPubKeyHash(MainNetParams.get(),paymentAddr.getPubKeyHash());
     }
 
     private static DeterministicKey derivePub(DeterministicKey pubKey, int index) {
